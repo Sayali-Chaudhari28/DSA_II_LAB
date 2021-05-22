3ad60233cf56052d97b70462b4e89678696c0bf9
Sayali Chaudhari <sayalinc2001@gmail.com>
2021-05-22 20:05:11 +0530

    Additional functions added 1. count number of leaf nodes 2. finding height of tree 3. finding parent node of the tree 4. removed inorder traversal code

--- src/Assignment2_2377.java
+++ src/Assignment2_2377.java
@@ -11,7 +11,7 @@ class tree{
 }
 class Binary_Search_Tree{
 	Scanner sc = new Scanner(System.in);
-	private tree root;
+	tree root;
 	String choice;
 	public Binary_Search_Tree() {
 		root = null;
@@ -103,14 +103,74 @@ class Binary_Search_Tree{
 		}
 		
 	}
-	void inorder_recursive(tree n) {
-		if(n!=null){
-			inorder_recursive(n.left);
-			System.out.print(n.data+", ");
-			inorder_recursive(n.right);
+	//-------------------Display tree in descending order--------------
+	void descending(tree r) {
+		if(r!=null){
+			descending(r.right);
+			System.out.print(r.data+" ");
+			descending(r.left);
 		}
 	}
-	
+	//------------------count number of nodes recursively--------------
+	int leaf(tree temproot){
+        if (temproot.left ==null && temproot.right==null){
+            return 1;
+        }
+        return  ( leaf(temproot.left) + leaf(temproot.right) ); 
+        
+    }
+	//------------------------find height of tree----------------------
+	int height(tree temproot) {
+		int lh, rh;
+        if(temproot.left==null){
+            lh=0;
+        }
+        else{
+            lh=1+height(temproot.left);
+        }
+      
+        if(temproot.right==null){
+            rh=0;
+        }
+        else{
+            rh=1+height(temproot.right);
+        }
+        
+        if(lh>rh)
+            return lh;
+        else
+            return rh;
+	}
+	//----------------------Parent node------------------------------
+	void parent_node() {
+		int value;
+        tree parent=null;
+        tree temp=root;
+        Scanner scan =new Scanner(System.in);
+        System.out.println("\nEnter child node whose parent is to be found:");//Accept a key whose parent is to be found
+        value=scan.nextInt();
+        if(value==root.data){       //if user enters root itself
+            System.out.println("\nNo parent as it is itself a parent of all elements");
+        }
+        else{
+            while(temp.data!=value && temp!=null){      //Traverse till leaf node
+                if(value>temp.data){
+                    parent=temp;
+                    temp=temp.right;        //Traverse ptr to right child
+                }
+                else{
+                    parent=temp;            //when value is less than data in temp
+                    temp=temp.left;         //Traverse ptr to left child
+                }
+            }
+            if(temp.data==value){       //given node is found
+                System.out.println("Parent of node"+ value +": "+ parent.data);      //display parent node
+            }
+            else{
+                System.out.println("Data not found");           //When temp==null
+            }
+        }
+	}
 }
 public class Assignment2_2377 {
 	public static void main(String args[]) {
@@ -123,7 +183,10 @@ public class Assignment2_2377 {
 					+ "\n3. Find Maximum node "
 					+ "\n4. Display tree Levelwise"
 					+ "\n5. Display tree in descinding order"
-					+ "\n6. Exit"
+					+ "\n6. Count number of leaf nodes "
+					+ "\n7. Find height of the tree "
+					+ "\n8. Parent node"
+					+ "\n9. Exit"
 					+ "\nEnter your choice: ");
 			choice = sc.nextInt();
 			switch(choice) {
@@ -133,11 +196,21 @@ public class Assignment2_2377 {
 				break;
 			case 3: bst.maximum();
 				break;
-			case 4:bst.display_level();
+			case 4: bst.display_level();
 				break;
-			case 5: 
+			case 5:if(bst.root == null)
+						System.out.println("Tree is Empty");
+					else
+						bst.descending(bst.root);
 				break;
-			case 6: System.exit(0);
+			case 6: System.out.println("Number of leaf nodes in the tree are: "+ bst.leaf(bst.root));
+				break;
+			case 7: int c = bst.height(bst.root); 
+				System.out.println("Height of the binary search tree is: "+ c);
+				break;
+			case 8: bst.parent_node();
+				break;
+			case 9: System.exit(0);
 				break;
 			}
 		}while(choice != 5);
